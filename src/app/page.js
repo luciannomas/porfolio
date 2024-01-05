8a3318d81f033d809cedb4774dbde916be57dc2b
luciannomas <luciano.mastran@gmail.com>
2024-01-05 06:03:36 -0300

    updated image pages

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,8 @@ import Container from '@mui/material/Container';
 
 import Front from './ui/Front/page';
 import Colors from './ui/Colors/page'
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import { useRouter } from "next/navigation";
 
 function Copyright(props) {
   return (
@@ -99,12 +101,14 @@ const footers = [
   },
 ];
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4, 5, 6];
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function Pricing() {
+  const router = useRouter();
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
@@ -117,8 +121,9 @@ export default function Pricing() {
       >
         <Toolbar sx={{ flexWrap: 'wrap' }}>
           <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-            Logo
+            <AccessTimeFilledIcon />
           </Typography>
+
           <nav>
             <Link
               variant="button"
@@ -183,20 +188,20 @@ export default function Pricing() {
                     // 16:9
                     pt: '56.25%',
                   }}
-                  image="https://source.unsplash.com/random?wallpapers"
+                  /* image="https://source.unsplash.com/random?wallpapers" */
+                  image="https://img.freepik.com/foto-gratis/concepto-programacion-navegacion-tecnologia-diseno-web_53876-163260.jpg?w=740&t=st=1704445110~exp=1704445710~hmac=2270d59fc77e8964df8ef4f171066dbfbfe649179ad9dbf50cd57a05c9a1ff6c"
                 />
                 <CardContent sx={{ flexGrow: 1 }}>
                   <Typography gutterBottom variant="h5" component="h2">
-                    Heading
+                    Eccomerce
                   </Typography>
                   <Typography>
-                    This is a media card. You can use this section to describe the
-                    content.
+                    This is an ideal page to upload products for a store.
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small">View Demo</Button>
-                  <Button size="small">Pay</Button>
+                  <Button size="small" onClick={() => router.push('https://mui.com/material-ui/getting-started/templates/blog/')}>View Demo</Button>
+                  <Button size="small" onClick={() => router.push('https://mui.com/material-ui/getting-started/templates/checkout/')}>Pay</Button>
                 </CardActions>
               </Card>
             </Grid>
