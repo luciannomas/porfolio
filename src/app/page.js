30a30cf9b22d1a47026e3eb8ab2605e574067f2a
luciannomas <luciano.mastran@gmail.com>
2024-01-10 17:44:49 -0300

    testing page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,3 @@
-"use client"
-import { createTheme} from '@mui/material/styles';
-
 //components
 import Front from './ui/Front/page';
 import Colors from './ui/Colors/page';
@@ -8,50 +5,6 @@ import MediaCard from './ui/Card/page';
 import Layout from './ui/Layout/page';
 import Title from './ui/Title/page';
 import Pricing from './ui/Pricing/page';
-
-
-const tiers = [
-  {
-    title: 'Static desing',
-    price: '50',
-    description: [
-      '10 users included',
-      '2 GB of storage',
-      'Help center access',
-      'Email support',
-    ],
-    buttonText: 'Get started',
-    buttonVariant: 'outlined',
-  },
-  {
-    title: 'Pro desing ',
-    subheader: 'Most popular',
-    price: '100',
-    description: [
-      '20 users included',
-      '10 GB of storage',
-      'Help center access',
-      'Priority email support',
-    ],
-    buttonText: 'Get started',
-    buttonVariant: 'outlined', //contained
-  },
-  {
-    title: 'Enterprise',
-    price: '500',
-    description: [
-      '50 users included',
-      '30 GB of storage',
-      'Help center access',
-      'Phone & email support',
-    ],
-    buttonText: 'Get started',
-    buttonVariant: 'outlined',
-  },
-];
-
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
 
 export default function Home() {
   
