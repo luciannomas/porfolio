2e8c00110ff8e9bbf0ea761b7561e091dede1d17
luciannomas <luciano.mastran@gmail.com>
2024-01-15 20:08:14 -0300

    correction eslint4

--- src/app/ui/Information/CompanyCard/page.js
+++ src/app/ui/Information/CompanyCard/page.js
@@ -31,7 +31,7 @@ export default function CompanyCard({ company }) {
                     gutterBottom
                     variant="h5"
                 >
-                    {company.title}
+                    Squarespace
                 </Typography>
                 <Typography
                     align="center"
